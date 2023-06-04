6100e37d2acf1645f92f169d16202780224ea7c2
Alvinalia <133779368+Alvinalia@users.noreply.github.com>
2023-06-04 22:19:31 +0700

    Update PenyanyiController.php

--- app/Http/Controllers/PenyanyiController.php
+++ app/Http/Controllers/PenyanyiController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Penyanyi;
+use App\Models\Label;
 
 class PenyanyiController extends Controller
 
 {
     public function index()
     {
-        $Penyanyi = penyanyi::all();
+        $Penyanyi = Penyanyi::with(['Label'])->get();
         return view('penyanyi',['listPenyanyi' => $Penyanyi]);
     }
-
 }
