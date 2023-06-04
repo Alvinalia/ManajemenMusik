28866eebfee520a7cc3f025cad51beca5cc40e72
Alvinalia <133779368+Alvinalia@users.noreply.github.com>
2023-06-04 22:21:13 +0700

    Update Label.php

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -7,7 +7,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Label extends Model
 {
+
     use HasFactory;
 
+    protected $fillabel = [
+        'id',
+        'nama',
+        'alamat',
+        'penyayi_id',
+    ];
     protected $table = "label";
+    protected $tamestamps = false;
+
+    public function Penyanyi(){
+        return $this->hasOne(Penyanyi::class, "id", "penyanyi_id");
+    }
+    public function Musik(){
+        return $this->hasMany(Musik::class, "Label_id", "id");
+    }
+
 }
