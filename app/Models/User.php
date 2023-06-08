0ba2cbe8705fbe2ae3dc412ec7d7ae7327dd08f5
Alvinalia <133779368+Alvinalia@users.noreply.github.com>
2023-06-09 00:06:10 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'id',
+        'username',
+        'no_tlp',
+        'jenis_kelamin',
+        'alamat',
+        'password',
+        'level',
+    ];
+
+    protected $primaryKey = 'id';
+    public $timestamps = false;
 
     protected $table = "user";
 }
