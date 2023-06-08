86f11f7a82b11460d14922c2ddeb7a0cbe7ddd07
Alvinalia <133779368+Alvinalia@users.noreply.github.com>
2023-06-08 18:24:10 +0700

    Update User.blade.php
    finish

--- resources/views/User.blade.php
+++ resources/views/User.blade.php
@@ -1,6 +1,14 @@
 @include('navbarrr')
 @section('content')
+<div class="my-3">
+    <a href="create" class= "btn btn-dark ml-5">Add Data</a>
+</div>
 <h2>Daftar Lagu 2023 Slebew</h2>
+@if(Session::has('status'))
+    <div class="alert alert-success" role="alert">
+        {{Session::get('message')}}
+    </div>
+@endif
 <table class="table">
     <thead>
         <tr>
@@ -25,7 +33,14 @@
             <td>{{$data->alamat}}</td>
             <td>{{$data->password}}</td>
             <td>{{$data->level}}</td>
-
+            <td>
+            <a href="edit/{{$data->id}}" class="btn btn-info">Edit</a>
+            
+            <form class="d-inline" action="/User/{{$data->id}}" method="post">
+                    @csrf
+                    @method('delete')
+                    <button type="submit" name="submit" class="btn btn-danger" onclick="return confirm('mau dii apuss nii??')">Delete</button>
+                </form>
             
         </tr>
         @endforeach
