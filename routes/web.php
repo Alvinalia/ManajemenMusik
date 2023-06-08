15f4d1a8f8cad08f6ed888ea612443d8d3797882
Alvinalia <133779368+Alvinalia@users.noreply.github.com>
2023-06-08 22:06:07 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Session;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\GenreMusikController;
@@ -21,11 +23,21 @@ use App\Http\Controllers\MusikController;
 
 
 
-Route::get('/',[GenreMusikController::class,'index']);
+Route::get('/',[GenreMusikController::class,'index2']);
 Route::get('/Penyanyi',[PenyanyiController::class,'index']);
 Route::get('/Genre',[GenreController::class,'index']);
 Route::get('/GenreMusik',[GenreMusikController::class,'index']);
 Route::get('/Label',[LabelController::class,'index']);
+Route::get('/home',[GenreMusikController::class,'index']);
+
 Route::get('/User',[UserController::class,'index']);
+Route::get('/create',[UserController::class,'create']);
+Route::post('/User',[UserController::class,'store']);
+Route::get('/edit/{id}',[UserController::class,'edit']);
+Route::put('/User/{id}',[UserController::class,'update']);
+Route::delete('/User/{id}',[UserController::class,'destroy']);
+
+
 Route::get('/Musik',[MusikController::class,'index']);
+
 
