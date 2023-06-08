84901aa47516c850e450bfebda6b84eb1ad0c08a
Alvinalia <133779368+Alvinalia@users.noreply.github.com>
2023-06-09 00:08:22 +0700

    Update GenreMusik.php

--- app/Models/GenreMusik.php
+++ app/Models/GenreMusik.php
@@ -10,4 +10,8 @@ class GenreMusik extends Model
     use HasFactory;
 
     protected $table = "genre_musik";
+
+    public function genre_musik(){
+        return $this->belongsTo(Genre::class, "id_genre", "id");
+    }
 }
