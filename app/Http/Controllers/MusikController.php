532960d87d4a624093e6c74a258319f12a820396
Alvinalia <133779368+Alvinalia@users.noreply.github.com>
2023-06-09 00:04:36 +0700

    Update MusikController.php

--- app/Http/Controllers/MusikController.php
+++ app/Http/Controllers/MusikController.php
@@ -9,8 +9,11 @@ class MusikController extends Controller
 {
     public function index()
     {
-        $Musik = musik::all();
+        $Musik = Musik::all();
         return view('musik',['listMusik' => $Musik]);
+
+        $GenreMusik = GenreMusik::with('id_musik')->get(); 
+        return view('genremusik',['listGenreMusik' => $GenreMusik]);
     }
 
 }
